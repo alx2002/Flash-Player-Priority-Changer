3b9768526051c0d635b64067c6f843299e9435ff
Alex <44152140+alx2002@users.noreply.github.com>
2018-10-20 13:28:42 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Text;
-using System.Threading.Tasks;
 
-// #define DEBUG
+
 
 namespace Processhighlevel
 {
@@ -17,55 +14,38 @@ namespace Processhighlevel
 
         static void Main(string[] args)
         {
-#if DEBUG
-            // File must be named flashplayer_31_sa
+
             Regex regex = new Regex(@"(?<![\w\d])flashplayer(_([0-9]+)_sa)*(?![\w\d])");
-            // Get all processes and use Linq to filter on the regex pattern
+            // name must be flashplayer_1_sa
+
             var processes = Process.GetProcesses().Where(p => regex.IsMatch(p.ProcessName));
-#endif
-            
-#if DEBUG
+
+
             Console.ForegroundColor = ConsoleColor.Cyan;
-           
-#endif
+
+
             foreach (var proc in processes)
             {
-#if DEBUG
 
-#endif
-               
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                Console.WriteLine("Set Priority for id: " + proc.Id + " to " + newPriority.ToString());
+
+
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Set Priority for PID: " + proc.Id + " to " + newPriority.ToString());
                 proc.PriorityClass = newPriority;
-             
-#if DEBUG
-                PutDebug("Changed priority for \n" + proc.Id);
-#endif
+
+
+                
+
             }
-#if DEBUG
-            PutDebug("No more processes.. \n");
-#endif      
+
+
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Press any key to exit... \n");
             Console.ReadKey(true);
         }
 
-        private static void PutDebug(object p)
-        {
-            throw new NotImplementedException();
-        }
-
-#if DEBUG
-        static bool debug = false;
-        static int debugInc = 1;
-        static void PutDebug(string info = "")
-        {
-            if (debug)
-            {
-                Console.WriteLine(" " + debugInc + ": " + info);
-                debugInc++;
-            }
-        }
-#endif
     }
+
 }
