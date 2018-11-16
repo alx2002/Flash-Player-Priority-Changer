6d9b226c630ff9671142e6ea6908043a1265be3c
Alex <44152140+alx2002@users.noreply.github.com>
2018-11-15 21:32:43 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,16 +3,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-
-
 namespace Processhighlevel
 {
-
     class Find_Process
     {
         static ProcessPriorityClass newPriority = ProcessPriorityClass.High; //
-        
-
+       
         static void Main(string[] args)
         {
 
@@ -20,27 +16,17 @@ namespace Processhighlevel
             // name must be flashplayer_1_sa
 
             var processes = Process.GetProcesses().Where(p => regex.IsMatch(p.ProcessName));
-
-
+            
             Console.ForegroundColor = ConsoleColor.Cyan;
-
-
+            
             foreach (var proc in processes)
             {
-
-
-
 
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("Set Priority for PID: " + proc.Id + " to " + newPriority.ToString());
                 proc.PriorityClass = newPriority;
 
-
-                
-
             }
-
-
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Press any key to exit... \n");
